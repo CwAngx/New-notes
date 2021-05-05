16ef63a515f55dec74ace7d521ecef87add034b2
陈王翾 <524884173@qq.com>
2021-05-05 23:11:22 +0800

    在杨氏矩阵查找指定元素

---
+++ Fundnum.c
@@ -0,0 +1,42 @@
+//杨氏矩阵查找元素
+#include<stdio.h>
+int Fundnum(int arr[3][3], int k, int* px, int* py)
+{
+	int x = 0;
+	int y = *py-1;
+	while (x <= *px && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 8;//要寻找的元素
+	int x = 3;
+	int y = 3;
+	int r = Fundnum(arr,k,&x,&y);//传址，可以在函数内部修改值，方便后续打印坐标
+	if (r == 0)
+	{
+		printf("没找到");
+	}
+	else
+	{
+		printf("找到了，坐标是%d %d\n",x,y);
+	}
+	return 0;
+}
